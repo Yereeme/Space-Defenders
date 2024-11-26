603280eaa0a4535afaefcd6c597291c2bce32ede
Infernape26 <imani.mobley17@gmail.com>
2024-11-26 12:01:30 -0500

    Update-11/26(2)

--- Connection/WindowControl.gd
+++ Connection/WindowControl.gd
@@ -1,8 +1,6 @@
 extends Node
 
 func _process(_delta):
-	if Input.is_action_just_pressed("Quit Game"):
-		get_tree().quit()
 	if Input.is_action_just_pressed("FullScreen"):
 		if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
